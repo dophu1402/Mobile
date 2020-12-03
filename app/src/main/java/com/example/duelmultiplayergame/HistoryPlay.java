494489125e8559b8d823dc4154ee55b1c967d332
Sgray <phongnguyen6120@gmail.com>
2020-12-03 16:36:07 +0700

    Update HistoryPlay.java

--- app/src/main/java/com/example/duelmultiplayergame/HistoryPlay.java
+++ app/src/main/java/com/example/duelmultiplayergame/HistoryPlay.java
@@ -1,8 +1,32 @@
 package com.example.duelmultiplayergame;
 
+import androidx.annotation.NonNull;
+
 import java.util.List;
 
+class Step{
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
+
 public class HistoryPlay {
-    List<Integer, Integer> step;
+    List<Step> step;
     
 }
